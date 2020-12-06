f38e699c5d0e58bed22cad8d6381808380280358
GoldenNerd <jmmejia4670@gmail.com>
2020-12-05 22:31:31 -0400

    Add files via upload

---
+++ template/Example_templateX.js
@@ -0,0 +1,12 @@
+"use strict"
+// RESET BUTTON:
+const reloadPage = document.getElementById("reload");
+reloadPage.addEventListener("click", () => location.reload());
+
+// VARIANT 1
+function runMethod() {
+
+
+};
+const runExample = document.getElementById("run-method-btn");
+runExample.addEventListener("click", runMethod);
